3da3d48a12c7df87aa0dd1062355c92aa9af7c06
uxcodev <nickatodd@gmail.com>
2023-02-28 17:53:37 -0800

    Update index.js

--- vue/index.js
+++ vue/index.js
@@ -10,6 +10,6 @@ app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)

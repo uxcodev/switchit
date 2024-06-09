7b1512b2e60ca132181c641633c16ce1cc6bc42b
Nick <nickatodd@gmail.com>
2024-06-09 14:09:12 -0700

    offers

--- vue/src/api/switchit.js
+++ vue/src/api/switchit.js
@@ -561,5 +561,21 @@ export default {
     } catch (err) {
       console.error(err);
     }
+  },
+
+  async updateOffer(id, body) {
+    try {
+      console.log(id, body)
+      // throw an error
+      throw new Error("NOT_IMPLEMENTED")
+
+
+      // let url = `/api/v1/offers/${id}/`;
+      // const response = await _axios.put(url, body);
+      // return response.data;
+    } catch (err) {
+      toast_error.show({ message: "Updating offers is not implemented yet" })
+      console.error(err);
+    }
   }
 }
